f123b552b38a642f32e07c9f89389c6bcdebbc35
Jorge Hecherat <hecherat@gmail.com>
2020-06-01 18:45:55 -0300

    [develop] - Axios added to project and first integration with api done

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Jorge' }}>
+    <AuthProvider>
       <SignIn />
       {/* <SignUp /> */}
-    </AuthContext.Provider>
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
