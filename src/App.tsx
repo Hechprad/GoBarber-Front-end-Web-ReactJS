be3300ba9dfcfb8a2d4580bd2f949c378344402b
Jorge Hecherat <hecherat@gmail.com>
2020-06-03 21:41:29 -0300

    [develop] - AppProvider done and added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,17 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks/index';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import Toastcontainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
       {/* <SignUp /> */}
-    </AuthProvider>
-
-    <Toastcontainer />
+    </AppProvider>
 
     <GlobalStyle />
   </>
